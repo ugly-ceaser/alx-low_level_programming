18b759f0b9c83af720cfef6bb201cb12d7e8e70e
Ugly_Ceaser <martins.paraclet@gmail.com>
2023-03-20 15:52:57 -0700

    my second commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #include "dog.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code for ALX students.
  *
  * Return: Always 0.
  */
@@ -10,9 +10,9 @@ int main(void)
 {
   struct dog my_dog;
 
-  my_dog.name = "Django";
+  my_dog.name = "DotNet";
   my_dog.age = 3.5;
-  my_dog.owner = "Jay";
+  my_dog.owner = "Ceaser";
   printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
   return (0);
 }
