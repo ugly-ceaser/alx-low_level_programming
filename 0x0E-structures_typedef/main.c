5076dc0eac8232c7e3c4f3b5f050e56b3acd036c
Ugly_Ceaser <martins.paraclet@gmail.com>
2023-03-20 15:49:55 -0700

    my first commit

---
+++ 0x0E-structures_typedef/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - check the code for ALX students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  struct dog my_dog;
+
+  my_dog.name = "DotNet";
+  my_dog.age = 6.0;
+  my_dog.owner = "Ceaser";
+  printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+  return (0);
+}
